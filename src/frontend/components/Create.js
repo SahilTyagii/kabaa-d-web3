547a773964ef3eb145a77b1cceaf142509ee9dbc
SahilTyagii <tyagi28sahil@gmail.com>
2024-04-23 14:21:42 +0530

    integrated lighthouse ipfs

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
 import { useState } from 'react'
 import { ethers } from "ethers"
 import { Row, Form, Button } from 'react-bootstrap'
-// import { create as ipfsHttpClient } from 'ipfs-http-client'
-// const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+import lighthouse from '@lighthouse-web3/sdk';
 
 const Create = ({ marketplace, nft }) => {
   const [fileImg, setFile] = useState(null);
@@ -11,125 +9,40 @@ const Create = ({ marketplace, nft }) => {
   const [desc, setDescription] = useState("")
   const [price, setPrice] = useState("")
   const [loading, setLoading] = useState(false);
-  // const uploadToIPFS = async (event) => {
-  //   event.preventDefault()
-  //   const file = event.target.files[0]
-  //   if (typeof file !== 'undefined') {
-  //     try {
-  //       const result = await client.add(file)
-  //       console.log(result)
-  //       setImage(`https://ipfs.infura.io/ipfs/${result.path}`)
-  //     } catch (error){
-  //       console.log("ipfs image upload error: ", error)
-  //     }
-  //   }
-  // }
 
-  ////////////////////////////////////////////////////////
+  const progressCallback = (progressData) => {
+    let percentageDone = 100 - (progressData?.total / progressData?.uploaded)?.toFixed(2)
+    console.log(percentageDone)
+  }
 
-
-
-  const sendJSONtoIPFS = async (ImgHash) => {
-
+  const uploadFile = async (file) => {
+    let output;
     try {
-
-      const resJSON = await axios({
-        method: "post",
-        url: "https://api.pinata.cloud/pinning/pinJsonToIPFS",
-        data: {
-          "name": name,
-          "description": desc,
-          "image": ImgHash
-        },
-        headers: {
-          'pinata_api_key': "40d4400089d220770050",
-          'pinata_secret_api_key': "c4ae5c39aa79c3aec20cf95ea63c317419da0715053f99f5de4a9f0d1d6234ab",
-
-        },
-      });
-
-      // https://gateway.pinata.cloud/ipfs/QmZ6iZAhazHyakzynC4sxZ6r6cikJmS69mZaCoyburKuq
-
-
-      const tokenURI = `https://gateway.pinata.cloud/ipfs/${resJSON.data.IpfsHash}`;
-      console.log("Token URI", tokenURI);
-      //mintNFT(tokenURI, currentAccount)   // pass the winner
-      mintThenList(tokenURI)
+      output = await lighthouse.upload(file, "93949fb2.0c176f0ee1ee45f9af63a00c701efee8", false, null, progressCallback);
+      console.log('File Status:', output);
+      console.log('Visit at https://gateway.lighthouse.storage/ipfs/' + output.data.Hash);
     } catch (error) {
-      console.log("JSON to IPFS: ")
-      console.log(error);
+      console.error("Upload to IPFS error:", error);
+      throw error; // Rethrow the error to be caught by the caller
     }
-
-
+    return output;
   }
 
-
-  ////////////////////////////////////////////////////////
-
-  const sendFileToIPFS = async (e) => {
-    e.preventDefault();
-  
-    if (fileImg) {
-      setLoading(true);
-      try {
-        const formData = new FormData();
-        formData.append("file", fileImg);
-        const resFile = await axios({
-          method: "post",
-          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-          data: formData,
-          headers: {
-            'pinata_api_key': "40d4400089d220770050",
-            'pinata_secret_api_key': "c4ae5c39aa79c3aec20cf95ea63c317419da0715053f99f5de4a9f0d1d6234ab",
-            "Content-Type": "multipart/form-data"
-          },
-        });
-  
-        const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
-        console.log("File uploaded to IPFS:", ImgHash);
-        sendJSONtoIPFS(ImgHash)
-  
-        // Reset form fields after successful upload
-        setFile(null);
-        setName("");
-        setDescription("");
-        setPrice("");
-      } catch (error) {
-        console.error("File to IPFS error:", error);
-        // Add error handling here
-      } finally {
-        setLoading(false);
-      }
-    } else {
-      console.error("No file selected.");
-      // Add error handling here
+  const createNFT = async () => {
+    if (!fileImg || !price || !name || !desc) return;
+    try {
+      const output = await uploadFile(fileImg);
+      const uri = `https://gateway.lighthouse.storage/ipfs/${output.data.Hash}`;
+      await (await nft.mint(uri)).wait();
+      const id = await nft.tokenCount();
+      await (await nft.setApprovalForAll(marketplace.address, true)).wait();
+      const listingPrice = ethers.utils.parseEther(price.toString());
+      await (await marketplace.makeItem(nft.address, id, listingPrice)).wait();
+    } catch (error) {
+      console.log("Create NFT error: ", error)
     }
   }
-  
 
-  ////////////////////////////////////////////////////////
-  // const createNFT = async () => {
-  //   if (!image || !price || !name || !description) return
-  //   try{
-  //     sendJSONtoIPFS(image)
-  //     // const result = await client.add(JSON.stringify({image, price, name, description}))
-  //     // mintThenList(result)
-  //   } catch(error) {
-  //     console.log("ipfs uri upload error: ", error)
-  //   }
-  // }
-  const mintThenList = async (uri) => {
-    // const uri = `https://ipfs.infura.io/ipfs/${result.path}`
-    // mint nft 
-    await (await nft.mint(uri)).wait()
-    // get tokenId of new nft 
-    const id = await nft.tokenCount()
-    // approve marketplace to spend nft
-    await (await nft.setApprovalForAll(marketplace.address, true)).wait()
-    // add nft to marketplace
-    const listingPrice = ethers.utils.parseEther(price.toString())
-    await (await marketplace.makeItem(nft.address, id, listingPrice)).wait()
-  }
   return (
     <div className="container-fluid mt-5">
       <div className="row">
@@ -146,7 +59,7 @@ const Create = ({ marketplace, nft }) => {
               <Form.Control onChange={(e) => setDescription(e.target.value)} size="lg" required as="textarea" placeholder="Description" />
               <Form.Control onChange={(e) => setPrice(e.target.value)} size="lg" required type="number" placeholder="Price in ETH" />
               <div className="d-grid px-0">
-                <Button onClick={sendFileToIPFS} variant="success" size="lg">
+                <Button onClick={createNFT} variant="success" size="lg">
                   Create & List NFT!
                 </Button>
               </div>
@@ -158,4 +71,4 @@ const Create = ({ marketplace, nft }) => {
   );
 }
 
-export default Create
+export default Create;
